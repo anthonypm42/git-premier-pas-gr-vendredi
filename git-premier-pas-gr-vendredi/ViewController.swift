37379084b295e0adde6d7691e7a26b448da56c82
git <git@LD140-21-06268.local>
2017-10-20 09:53:03 -0400

    Fusion des 2 lignes 15

--- git-premier-pas-gr-vendredi/ViewController.swift
+++ git-premier-pas-gr-vendredi/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Je suis la ligne 15")
+        print("Je suis aussi la ligne 15")
         // Do any additional setup after loading the view, typically from a nib.
         print("viewDidLoad")
         afficher("Bonjour git", fois: 5)
